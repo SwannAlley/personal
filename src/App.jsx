4fd8512fda162bbbc223db9e26aeeff6bf274182
Alley <llzeynolla@vk.com>
2025-05-14 14:37:24 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+        const App = () => {
+            return (
+                <div className="m-2 w-1/2 aspect-square border-4 border-black rounded-full">
+                    <h1 className="w-full text-center">Али Зейнолла</h1>
+                    <p>Weird, beautiful, and interesting web design</p>
+                </div>
+        )
+        }
+        export default App
