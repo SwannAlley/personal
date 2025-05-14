89a4c627112ccb1f134adac9778b6147b51519c5
Alley <llzeynolla@vk.com>
2025-05-14 15:13:08 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
         const App = () => {
             return (
-                <div className="m-2 w-1/2 aspect-square border-4 border-black rounded-full">
-                    <h1 className="w-full text-center">Али Зейнолла</h1>
-                    <p>Weird, beautiful, and interesting web design</p>
+                <div className="w-full">
+                    <div className="flex flex-col justify-center h-screen w-full text-center">
+                        <h1 className="text-black font-bold py-1 px-4 uppercase fontsize-64">Али Зейнолла</h1>
+                        <h3 className="px-4 link_text_color hover:underline"><a href="https://buymeacoffee.com">купить мне кофе</a></h3>
+                    </div>
+                    <div className="flex flex-col justify-center h-screen w-full text-center">
+                        <a></a>
+                    </div>
                 </div>
         )
         }
